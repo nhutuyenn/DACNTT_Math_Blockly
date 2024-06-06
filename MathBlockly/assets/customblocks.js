b24471efef27cc53984a242f862b915dd27a30b3
Tuyen Nguyen <520h0594@student.tdtu.edu.vn>
2024-06-07 00:42:29 +0700

    add block

--- MathBlockly/assets/customblocks.js
+++ MathBlockly/assets/customblocks.js
@@ -1,30 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
+    // Define the custom block
+    Blockly.Blocks['block'] = {
+        init: function () {
+            this.appendDummyInput()
+                .appendField("Hình vuông");
+            this.appendValueInput("NAME")
+                .setCheck(null)
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("Hình ảnh:");
+            this.setColour(180);
+            this.setTooltip("");
+            this.setHelpUrl("");
+        }
+    };
 
-// Define the custom block
-Blockly.Blocks['block'] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField("Hình vuông");
-        this.appendValueInput("NAME")
-            .setCheck(null)
-            .setAlign(Blockly.ALIGN_RIGHT)
-            .appendField("Hình ảnh:");
-        this.setColour(180);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
-};
+    Blockly.Blocks['image'] = {
+        init: function () {
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage("https://monka.vn/content/img/upload/u21-11-2018-09-45-16.png", 80, 80, { alt: "*", flipRtl: "FALSE" }));
+            this.setOutput(true, null);
+            this.setColour(165);
+            this.setTooltip("");
+            this.setHelpUrl("");
+        }
+    };
 
-var workspace = Blockly.inject('blocklyDiv', {
-    toolbox: '<xml><block type="block"></block></xml>'
-});
+    var workspace = Blockly.inject('blocklyDiv', {
+    });
 
-// Define the custom block's JavaScript code generation
-Blockly.JavaScript['block'] = function (block) {
-    var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-    var code = 'Hello ' + value_name + '\n';
-    return code;
-};
+    var newBlock = workspace.newBlock('block');
+    newBlock.initSvg();
+    newBlock.render();
+
+    var imageBlock = workspace.newBlock('image');
+    imageBlock.initSvg();
+    imageBlock.render();
+
+    // Define the custom block's JavaScript code generation
+    Blockly.JavaScript['block'] = function (block) {
+        var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+        var code = 'Hello ' + value_name + '\n';
+        return code;
+    };
 
 });
