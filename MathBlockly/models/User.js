0682229c60d58b0b7fe247bef874a53cb0d937ae
Tuyen Nguyen <520h0594@student.tdtu.edu.vn>
2024-06-22 13:00:51 +0700

    user details and classroom

--- MathBlockly/models/User.js
+++ MathBlockly/models/User.js
@@ -1,47 +1,72 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, 'Username is required'],
-    unique: [ true, 'Username already exists'],
+    unique: [true, 'Username already exists'],
   },
   email: {
     type: String,
     required: [true, 'Email is required'],
-    unique: [ true, 'Email already exists'],
+    unique: [true, 'Email already exists'],
     validate: {
-        validator: validator.isEmail,
-        message: 'Please enter a valid email'
-      }
+      validator: validator.isEmail,
+      message: 'Please enter a valid email'
+    }
   },
   password: {
     type: String,
     required: [true, 'Password is required'],
     minlength: [6, 'Password must be at least 6 characters']
-  }
+  },
+  name: {
+    type: String,
+    required: [true, 'Username is required'],
+  },
+
+  phone: {
+    type: String,
+  },
+
+  dateBirth: {
+    type: String,
+  },
+
+  school: {
+    type: String,
+  },
+
+  active: {
+    type: Boolean
+  },
+
+  classroomID: [{
+    type: Schema.Types.ObjectId,
+  }]
 });
 
 // Pre-save hook to hash password
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 8);
   }
   next();
 });
 
-userSchema.static('login', async function(username, password) {
-    const user = await this.findOne({ username });
-    if (user) {
-        const auth = await bcrypt.compare(password, user.password);
-        if (auth) {
-            return user;
-        }
-        throw new Error('incorrect password'); // Không để lộ chi tiết
+userSchema.static('login', async function (username, password) {
+  const user = await this.findOne({ username });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw new Error('incorrect username');
+    throw new Error('incorrect password'); // Không để lộ chi tiết
+  }
+  throw new Error('incorrect username');
 });
 
 const User = mongoose.model('User', userSchema);
